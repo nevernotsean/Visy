457c478e224df635db4ae4b92be2dbf90c3698ce
Sean Harrington <sean.harrington@martinagency.com>
2019-04-19 14:13:22 -0400

    add design reference, deformers, mp3 assets, stripes scene

---
+++ Assets/AudioDeformAmplitude.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Deform;
+
+public class AudioDeformAmplitude : MonoBehaviour {
+    private RippleDeformer deform;
+    public UnityAtoms.FloatVariable Input;
+    public float multiplier;
+    // public Transform originalTransform;
+
+    private float originalAmplitude;
+
+    void Start () {
+        deform = GetComponent<RippleDeformer> ();
+        // originalTransform = transform;
+        originalAmplitude = deform.Amplitude;
+    }
+
+    void Update () {
+        deform.Amplitude = originalAmplitude * Input.Value * multiplier;
+    }
+}
---
+++ Assets/AudioTrackerBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms;
+
+public class AudioTrackerBehavior : MonoBehaviour {
+    public float lowInput { get; set; }
+    public float midInput { get; set; }
+    public float highInput { get; set; }
+
+    public FloatVariable SpecrumLow;
+    public FloatVariable SpecrumMid;
+    public FloatVariable SpecrumHigh;
+    // Start is called before the first frame update
+    void Start () { }
+
+    // Update is called once per frame
+    void Update () {
+        SpecrumLow.SetValue (lowInput);
+        SpecrumMid.SetValue (midInput);
+        SpecrumHigh.SetValue (highInput);
+    }
+}
